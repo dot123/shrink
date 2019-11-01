5301e9d86237dcb588f18bb4c479f5d3751f1e60
dot123 <conjurer888888@gmail.com>
2019-11-02 00:26:47 +0800

    initial

---
+++ assets/Script/Helloworld.ts
@@ -0,0 +1,165 @@
+import RuleData from "./RuleData";
+
+const { ccclass, property } = cc._decorator;
+
+@ccclass
+export default class Helloworld extends cc.Component {
+    @property(cc.Node)
+    private background: cc.Node = null;
+
+    @property(cc.Node)
+    private targetNode: cc.Node = null;
+
+    @property(cc.Node)
+    private leftNode: cc.Node = null;
+
+    @property(cc.Node)
+    private rightNode: cc.Node = null;
+
+    @property(cc.Node)
+    private bottomNode: cc.Node = null;
+
+    @property(cc.Node)
+    private topNode: cc.Node = null;
+
+    @property(cc.Label)
+    private timeLabel: cc.Label = null;
+
+    private leftSpeed: number = 0;
+    private rightSpeed: number = 0;
+    private bottomSpeed: number = 0;
+    private topSpeed: number = 0;
+
+    private staticTime: number = 0; //静止时间
+    private moveTime: number = 0; //移动时间
+    private timeCount: number = 0;
+    private isStart: boolean = false; //是否开始
+
+    private count: number = 0;
+    private scale: number = 0;
+
+    private isEnd: boolean = false;
+
+    private with: number = 0;
+    private height: number = 0;
+    private offsetY: number = 0;
+
+    start() {
+        let w = 960; //地图大小
+        let h = 640;
+
+        this.background.setContentSize(w, h);
+
+        this.leftNode.setContentSize(w, h);
+        this.rightNode.setContentSize(w, h);
+        this.bottomNode.setContentSize(w, h);
+        this.topNode.setContentSize(w, h);
+
+        this.leftNode.setPosition(-w / 2, 0);
+        this.rightNode.setPosition(w / 2, 0);
+        this.bottomNode.setPosition(0, -h / 2);
+        this.topNode.setPosition(0, h / 2);
+
+        this.targetNode.setContentSize(w, h);
+
+        this.Setup();
+    }
+
+    update(dt) {
+        if (this.isEnd) {
+            return;
+        }
+
+        if (this.isStart) {
+            this.leftNode.x -= this.leftSpeed;
+            this.rightNode.x -= this.rightSpeed;
+            this.bottomNode.y -= this.bottomSpeed;
+            this.topNode.y -= this.topSpeed;
+
+            this.with = this.rightNode.x - this.leftNode.x;
+            this.height = this.topNode.y - this.bottomNode.y;
+
+            this.leftNode.height = this.height;
+            this.leftNode.y += this.offsetY;
+            this.rightNode.height = this.height;
+            this.rightNode.y += this.offsetY;
+
+            this.timeCount++;
+
+            this.timeLabel.string = "移动时间:" + this.timeCount;
+
+            if (this.timeCount >= this.moveTime) {
+                this.timeCount = 0;
+                this.isStart = false;
+
+                this.Setup(); //设置下次
+            }
+        } else {
+            this.timeCount++;
+
+            this.timeLabel.string = "静止时间:" + this.timeCount;
+
+            if (this.timeCount >= this.staticTime) {
+                this.timeCount = 0;
+                this.isStart = true;
+
+                let size = this.targetNode.getContentSize();
+                let curPos = this.targetNode.position;
+
+                let w = size.width * (1 - this.scale);
+                let h = size.height * (1 - this.scale);
+                let w2 = w / 2;
+                let h2 = h / 2; //锚点在0.5,0.5
+
+                let y = this.RandInt(-h2, h2);
+
+                this.offsetY = y / this.moveTime;
+                this.targetNode.setContentSize(size.width - w, size.height - h);
+                this.targetNode.position = cc.v2(curPos.x + this.RandInt(-w2, w2), curPos.y + y);
+
+                this.CalcuSpeed();
+            }
+        }
+    }
+
+    private CalcuSpeed() {
+        let xMin = this.targetNode.x - this.targetNode.width / 2;
+        let xMax = this.targetNode.x + this.targetNode.width / 2;
+        let yMin = this.targetNode.y - this.targetNode.height / 2;
+        let yMax = this.targetNode.y + this.targetNode.height / 2;
+
+        let leftDistance = this.leftNode.x - xMin; //距离
+        let rightDistance = this.rightNode.x - xMax;
+        let bottomDistance = this.bottomNode.y - yMin;
+        let topDistance = this.topNode.y - yMax;
+
+        this.leftSpeed = leftDistance / this.moveTime; //移动速度
+        this.rightSpeed = rightDistance / this.moveTime;
+        this.bottomSpeed = bottomDistance / this.moveTime;
+        this.topSpeed = topDistance / this.moveTime;
+    }
+
+    private Setup() {
+        this.with = this.targetNode.width;
+        this.height = this.targetNode.height;
+
+        this.count++;
+        let info = RuleData[this.count];
+        if (!info) {
+            this.isEnd = true;
+            return;
+        }
+        this.staticTime = info["staticTime"];
+        this.moveTime = info["moveTime"];
+        this.scale = info["scale"];
+    }
+
+    private RandInt(start, end) {
+        return Math.random() * (end + 1 - start) + start;
+    }
+
+    public GetRect() {
+        let pos = this.targetNode.position;
+        return new cc.Rect(pos.x, pos.y, this.with, this.height);
+    }
+}
---
+++ assets/Script/RuleData.ts
@@ -0,0 +1,52 @@
+const RuleData = {
+    1: {
+        staticTime: 300,
+        moveTime: 300,
+        damage: 0.4,
+        scale: 0.4
+    },
+    2: {
+        staticTime: 200,
+        moveTime: 140,
+        damage: 0.6,
+        scale: 0.65
+    },
+    3: {
+        staticTime: 150,
+        moveTime: 90,
+        damage: 0.8,
+        scale: 0.5
+    },
+    4: {
+        staticTime: 120,
+        moveTime: 60,
+        damage: 1,
+        scale: 0.5
+    },
+    5: {
+        staticTime: 120,
+        moveTime: 40,
+        damage: 3,
+        scale: 0.5
+    },
+    6: {
+        staticTime: 90,
+        moveTime: 30,
+        damage: 5,
+        scale: 0.5
+    },
+    7: {
+        staticTime: 90,
+        moveTime: 30,
+        damage: 7,
+        scale: 0.5
+    },
+    8: {
+        staticTime: 60,
+        moveTime: 30,
+        damage: 9,
+        scale: 0.5
+    }
+};
+
+export default RuleData;
